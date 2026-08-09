367c6be99b555e60a64d024b41908c84426b4471
Ginabo Official <Ginabo.official@gmail.com>
2026-08-09 19:33:02 +0700

    auto: apps/web/src/app/layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -47,6 +47,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id" className={`${montserrat.variable} ${poppins.variable} ${staatliches.variable} ${outfit.variable}`} suppressHydrationWarning>
       <body className="font-poppins" suppressHydrationWarning>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-1X51JZSBM2" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1X51JZSBM2');
+          `}
+        </Script>
         <AuthProvider>
           <CartProvider>
             <ClientShell>
